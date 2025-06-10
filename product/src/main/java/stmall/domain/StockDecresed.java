75496d82d973bc29844b5a0a2f10489092a02901
dnjsdn752 <134488624+dnjsdn752@users.noreply.github.com>
2025-06-10 13:34:14 +0900

    commit

---
+++ product/src/main/java/stmall/domain/StockDecresed.java
@@ -0,0 +1,26 @@
+package stmall.domain;
+
+import java.time.LocalDate;
+import java.util.*;
+import lombok.*;
+import stmall.domain.*;
+import stmall.infra.AbstractEvent;
+
+//<<< DDD / Domain Event
+@Data
+@ToString
+public class StockDecresed extends AbstractEvent {
+
+    private Long id;
+    private String name;
+    private Integer stock;
+
+    public StockDecresed(Inventory aggregate) {
+        super(aggregate);
+    }
+
+    public StockDecresed() {
+        super();
+    }
+}
+//>>> DDD / Domain Event
